51295662ebb1016e050206d262cccc39d2425379
unimol <10322778+unimol@users.noreply.github.com>
2018-07-16 13:40:40 +0200

    Update data.agencies.js

--- js/data.agencies.js
+++ js/data.agencies.js
@@ -13,8 +13,35 @@ var agencies = {
 		, www: "https://www.schufa.de/de/datenschutz-dsgvo/"
 		, text: 4
 		, desc: "Auskunftei: SCHUFA"
+	},
+	"Bürgel": {
+		  title: "CRIF Bürgel GmbH"
+		, subtitle: "Datenschutzbeauftragte/r"
+		, street: "Gasstraße 18"
+		, zipcode: "22761"
+		, country: "Deutschland"
+		, city: "Hamburg"
+		, tel: "040 - 898030"
+		, fax: "040 - 89803309"
+		, law: "§ 35 Bundesdatenschutzgesetz (BDSG-neu) ff. Art. 17 DSGVO"
+		, mail: "datenschutz.de@crif.com"
+		, www: "https://www.crifbuergel.de/"
+		, text: 4
+		, desc: "Auskunftei: Bürgel"
+	},
+	"infoscore": {
+		  title: "infoscore Consumer Data GmbH"
+		, subtitle: "Abteilung Datenschutz"
+		, street: "Rheinstraße 99"
+		, zipcode: "76532"
+		, country: "Deutschland"
+		, city: "Baden-Baden"
+		, tel: "05241 - 5276000"
+		, fax: "07221 - 50403201"
+		, law: "§ 35 Bundesdatenschutzgesetz (BDSG-neu) ff. Art. 17 DSGVO"
+		, mail: "datenschutz.de@crif.com"
+		, www: "http://www.arvato-infoscore.de/verbraucher/selbstauskunft/"
+		, text: 4
+		, desc: "Auskunftei: infoscore"
 	}
-
-	
-
 };
