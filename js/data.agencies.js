0cbe711860328556539ecf9ee921d32d6296ee27
unimol <10322778+unimol@users.noreply.github.com>
2018-07-16 13:03:33 +0200

    Update data.agencies.js

--- js/data.agencies.js
+++ js/data.agencies.js
@@ -11,23 +11,10 @@ var agencies = {
 		, law: "§ 19, Abs. 1 Bundesdatenschutzgesetz (BDSG-neu) ff. Art. DSGVO"
 		, mail: "datenschutz@schufa.de"
 		, www: "https://www.schufa.de/de/datenschutz-dsgvo/"
-		, text: 2
+		, text: 3
 		, desc: "Schufa"
 	}
 
 	
-	, "LVS Baden-Wuerttemberg": {
-		  title: "Landesamt für Verfassungsschutz"
-		, street: "Taubenheimstr. 85A"
-		, zipcode: "70372"
-		, city: "Stuttgart"
-		, country: "Deutschland"
-		, tel: "0711 9544 - 00"
-		, fax: "0711 9544 - 444"
-		, mail: "info@lfvbw.bwl.de"
-		, www: "http://www.verfassungsschutz-bw.de/"
-		, law: "§ 13 Gesetz über den Verfassungsschutz in Baden-Württemberg § 21 Landesdatenschutzgesetz Baden-Würtemberg"
-		, text: 2
-		, desc: "Landesamt f&uuml;r Verfassungsschutz Baden-W&uuml;rttemberg"
-	}
+
 };
